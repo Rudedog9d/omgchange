38d26cafdcdc92fd86018737033142cea8b135f9
Brodie Davis <davisb5@sunyit.edu>
2017-02-05 00:50:51 -0500

    changed the way Watcher class gets imported to __init__.py

--- __init__.py
+++ __init__.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python
-from . import watcher
+import sys, os
+
+# TODO: Fix this hack? to import things
+# Add omgchange folder to syspath
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
+
+from omgchange.watcher import Watcher
